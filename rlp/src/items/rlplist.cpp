ab1054fa36b2fa76f989e669fa9034a2cda778c7
Rahul Shelke <srahul07@gmail.com>
2020-06-04 23:17:16 +0530

    Adding rlp library

---
+++ rlp/src/items/rlplist.cpp
@@ -0,0 +1,25 @@
+#include "include/rlplist.hpp"
+
+#include "include/utils.hpp"
+
+
+RLPList::RLPList(const buffer_t input) {
+    _bytes = input;
+}
+
+buffer_t RLPList::GetBytes() {
+    return _bytes;
+}
+
+// std::string RLPList::AsHexString() {
+//     return BytesToString(_bytes);
+// }
+
+RLPList RLPList::Create(const std::vector <uint64_t> input) {
+    return RLPList(input);
+}
+
+RLPList RLPList::Create(const std::string input[]) {
+    // Todo: Need to change this
+    return RLPList(EmptyByte());
+}
