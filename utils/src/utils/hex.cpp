68d328463241567a13dcdd7d9ddd8b43fb7eb543
Rahul Shelke <srahul07@gmail.com>
2020-06-05 17:58:57 +0530

    Moving utils to their own directory

--- utils/src/utils/hex.cpp
+++ utils/src/utils/hex.cpp
@@ -5,39 +5,41 @@
 #include <algorithm>
 
 #include "constants.hpp"
+#include "alias.hpp"
 
-buffer_t EmptyByte() {
+buffer_t utils::EmptyByte() {
     // buffer_t bytes_ = StringToBytes(std::to_string(EMPTY_STRING));
     // return bytes_;
     return StringToBytes(std::to_string(EMPTY_STRING));
 }
 
-bool IsNumeric(const std::string& input) {
+bool utils::IsNumeric(const std::string& input) {
     return std::all_of(input.begin(), input.end(), ::isdigit);
 }
 
-bool IsHexPrefixed(const std::string& input) {
+bool utils::IsHexPrefixed(const std::string& input) {
     return input.substr(0, 2) == "0x";
 }
 
-bool IsHexString(const std::string& input) {
+bool utils::IsHexString(const std::string& input) {
     bool flag_ = true;
     std::string stripped_string_ = StripHexPrefix(input);
 
     for(std::string::size_type i = 0; i < stripped_string_.length(); i++) {
-        if(!isxdigit(input[i])) {
+        if(!isxdigit(stripped_string_.at(i))) {
             flag_ = false;
             break;
         }
     }
+    
     return flag_;
 }
 
-std::string InsertHexPrefix(const std::string& input) {
+std::string utils::InsertHexPrefix(const std::string& input) {
     return "0x" + input;
 }
 
-std::string StripHexPrefix(const std::string& input) {
+std::string utils::StripHexPrefix(const std::string& input) {
     std::string result_ {input};
     if(IsHexPrefixed(input)) {
         result_ = input.substr(2, input.length());
@@ -45,11 +47,11 @@ std::string StripHexPrefix(const std::string& input) {
     return result_;
 }
 
-std::string PadToEven(const std::string& input) {
+std::string utils::PadToEven(const std::string& input) {
     return (input.length() % 2) ? "0" + input : input;
 }
 
-std::string IntegerToHex(const uint64_t input) {
+std::string utils::IntegerToHex(const uint64_t input) {
     if (input < 0) {
         // Todo: Throw an error
         return "";
@@ -61,13 +63,13 @@ std::string IntegerToHex(const uint64_t input) {
     return PadToEven(stream_.str());
 }
 
-uint64_t HexToInteger(const std::string& input) {
+uint64_t utils::HexToInteger(const std::string& input) {
     std::string hex_str_ = StripHexPrefix(input);
     uint64_t number = std::stoul(hex_str_, nullptr, 16);
     return number;
 }
 
-std::string StringToHex(const std::string& input, bool prefix, bool upper) {
+std::string utils::StringToHex(const std::string& input, bool prefix, bool upper) {
     std::ostringstream stream_;
 
     unsigned int converter_ {0};
@@ -81,7 +83,7 @@ std::string StringToHex(const std::string& input, bool prefix, bool upper) {
     return prefix ? InsertHexPrefix(stream_.str()) : stream_.str();
 }
 
-std::string HexToString(const std::string& input) {
+std::string utils::HexToString(const std::string& input) {
     std::string stream_ {""};
     std::string hex_str_ {""};
 
@@ -94,7 +96,7 @@ std::string HexToString(const std::string& input) {
     return stream_;
 }
 
-std::uint64_t SafeParseInt(const std::string& input, unsigned int base) {
+std::uint64_t utils::SafeParseInt(const std::string& input, unsigned int base) {
     if (input.substr(0, 2) == "00") {
         // Todo: Throw exception
         return 0;
@@ -103,7 +105,7 @@ std::uint64_t SafeParseInt(const std::string& input, unsigned int base) {
     return std::stoul(input, 0, base);
 }
 
-buffer_t IntegerToBytes(const uint64_t input) {
+buffer_t utils::IntegerToBytes(const uint64_t input) {
     buffer_t bytes_;
 
     std::ostringstream output_;
@@ -115,7 +117,7 @@ buffer_t IntegerToBytes(const uint64_t input) {
     return bytes_;
 }
 
-uint64_t BytesToInteger(const std::vector<char>& input) {
+uint64_t utils::BytesToInteger(const buffer_t& input) {
     uint64_t result_ {0};
     for(auto item_ : input) {
         result_ = result_ * 10 + item_;
@@ -123,7 +125,7 @@ uint64_t BytesToInteger(const std::vector<char>& input) {
     return result_;
 }
 
-buffer_t StringToBytes(const std::string& input) {
+buffer_t utils::StringToBytes(const std::string& input) {
     buffer_t bytes_;
     std::string hex_str_ {""};
     uint64_t hex_char_ {0};
@@ -144,11 +146,31 @@ buffer_t StringToBytes(const std::string& input) {
     return bytes_;
 }
 
-std::string BytesToString(const buffer_t& input) {
+std::string utils::BytesToString(const buffer_t& input) {
     std::string byte_str_ {""};
     for(std::string::size_type i = 0; i < input.size(); i++ ) {
         auto byte_ = input[i];
         byte_str_ += byte_;
     }
     return byte_str_;
+}
+
+buffer_t utils::ToBytes(const std::string& input) {
+    buffer_t bytes_;
+    if(input.empty()) {
+        // Empty string
+        bytes_ = EmptyByte();
+    } else if(IsNumeric(input)) {
+        // It's a positive number
+        auto number_ = SafeParseInt(input);
+        bytes_ = IntegerToBytes(number_);
+    } else {
+        if(IsHexString(input)) {
+            bytes_ = StringToBytes(PadToEven(StripHexPrefix(input)));
+        } else {
+            bytes_ = StringToBytes(input);
+        }
+    }
+
+    return bytes_;
 }
