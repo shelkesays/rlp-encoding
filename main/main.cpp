885250523c352be69e3db71ea58e52721ec917ca
Rahul Shelke <srahul07@gmail.com>
2020-06-05 17:59:31 +0530

    Adding main testing

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,30 @@
-// #include <iostream>
+#include <iostream>
 
-#include <say-hello/hello.hpp>
+#include <utils/hex.hpp>
+#include <rlp/items/rlpstring.hpp>
+#include <rlp/codec/rlpencode.hpp>
+#include <rlp/codec/rlpdecode.hpp>
+
 
 int main() {
-    hello::say_hello();
+    std::cout << utils::StringToBytes("Rahul").data() << std::endl;
+
+    // std::string original_str {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+    std::string original_str {"world"};
+    std::vector<uint64_t> encoded_str = RLPEncoder::EncodeString(original_str);
+    std::cout << RLPEncoder::GetBytes(encoded_str) << std::endl;
+
+    // std::string byte_str_ = BytesToString(encoded_str);
+    std::string new_str_ = RLPDecoder::DecodeByte(encoded_str);
+    std::cout << "Decoded: " << new_str_ << std::endl;
+
+    // RLPString test_ = RLPString::Create(0L);
+    // std::vector<uint64_t> encoded_str_ = RLPEncoder::EncodeString(test_);
+    // std::cout << RLPEncoder::GetBytes(encoded_str_) << std::endl;
+    
+    // std::string byte_str_ = BytesToString(encoded_str_);
+    // new_str_ = RLPDecoder::DecodeString(byte_str_);
+    // std::cout << "Decoded: " << new_str_ << std::endl;
 
     return 0;
 }
