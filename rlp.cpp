bcccc2a22240b135e004fe2fe6f3bd4eb8caae3e
Rahul Shelke <srahul07@gmail.com>
2020-06-25 17:07:34 +0530

    making changes as per new buffer

--- rlp.cpp
+++ rlp.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 
 #include <constants/alias.hpp>
-// #include <utils/hex.hpp>
-// #include <rlp/items/rlpstring.hpp>
-// #include <rlp/codec/rlpencode.hpp>
-// #include <rlp/codec/rlpdecode.hpp>
+#include <utils/hex.hpp>
+#include <rlp/items/rlpstring.hpp>
+#include <rlp/codec/rlpencode.hpp>
+#include <rlp/codec/rlpdecode.hpp>
 
 
 int main() {
-    std::cout << sizeof(uint_t) << std::endl;
-    // std::cout << verified::utils::StringToBytes("Rahul").data() << std::endl;
-    // std::cout << std::to_string(0x00) << std::endl;
-    // std::string original_str {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+    // std::cout << sizeof(uint_t) << std::endl;
+    std::cout << verified::utils::StringToBytes("Rahul").data() << std::endl;
+    std::cout << std::to_string(0x00) << std::endl;
+    std::string original_str {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
     // std::string original_str {"1✁g"};
     // std::string original_str {"world"};
-    // std::vector<uint64_t> encoded_str = rlp::RLPEncoder::EncodeString(original_str);
-    // std::cout << "Encoded: " << encoded_str.data() << std::endl;
-    // std::cout << rlp::RLPEncoder::GetBytes(encoded_str) << std::endl;
+    buffer_t encoded_str = verified::rlp::RLPEncoder::EncodeString(original_str);
+    std::cout << "Encoded: " << encoded_str.data() << std::endl;
+    std::cout << verified::rlp::RLPEncoder::GetBytes(encoded_str) << std::endl;
 
-    // std::string byte_str_ = verified::utils::BytesToString(encoded_str);
-    // std::string new_str_ = rlp::RLPDecoder::DecodeByte(encoded_str);
-    // std::cout << "Decoded: " << new_str_ << std::endl;
+    std::string byte_str_ = verified::utils::BytesToString(encoded_str);
+    std::string new_str_ = verified::rlp::RLPDecoder::DecodeByte(encoded_str);
+    std::cout << "Decoded: " << new_str_ << std::endl;
 
-    // rlp::RLPString test_ = rlp::RLPString::Create(0L);
-    // std::vector<uint64_t> encoded_str_ = rlp::RLPEncoder::EncodeString(test_);
-    // std::cout << rlp::RLPEncoder::GetBytes(encoded_str_) << std::endl;
+    verified::rlp::RLPString test_ = verified::rlp::RLPString::Create(0L);
+    buffer_t encoded_str_ = verified::rlp::RLPEncoder::EncodeString(test_);
+    std::cout << verified::rlp::RLPEncoder::GetBytes(encoded_str_) << std::endl;
     
-    // byte_str_ = verified::utils::BytesToString(encoded_str_);
-    // new_str_ = rlp::RLPDecoder::DecodeString(byte_str_);
-    // std::cout << "Decoded: " << new_str_ << std::endl;
+    byte_str_ = verified::utils::BytesToString(encoded_str_);
+    new_str_ = verified::rlp::RLPDecoder::DecodeString(byte_str_);
+    std::cout << "Decoded: " << new_str_ << std::endl;
 
     return 0;
 }
