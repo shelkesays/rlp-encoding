2acb63c518d323b148efe47fbdfe3effa936fd6d
Rahul Shelke <srahul07@gmail.com>
2020-06-25 12:10:08 +0530

    Updating unint64_t to unint_t and adding int and long

--- constants/src/constants/alias.hpp
+++ constants/src/constants/alias.hpp
@@ -9,10 +9,10 @@ using uint_t = unsigned int;
 
 using nibble_t = std::vector<uint_t>;
 
-using buffer_t = std::vector<uint64_t>;
+using buffer_t = std::vector<uint_t>;
 
 using buffer_array_t = std::vector<buffer_t>;
 
-using variant_t = boost::variant<long, uint64_t, std::string, buffer_t, buffer_array_t>;
+using variant_t = boost::variant<int, long, uint_t, uint64_t, std::string, buffer_t, buffer_array_t>;
 
 #endif
