0c27ba707277e8f9d4dcb838b7b53167215f6bcb
Rahul Shelke <srahul07@gmail.com>
2020-08-21 19:34:00 +0530

    Adding element type enum file

---
+++ constants/src/constants/elementtype.hpp
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_ELEMENT_TYPE_H
+#define CONSTANTS_ELEMENT_TYPE_H
+
+enum ElementType {
+    CHAR = 0,
+    INT = 1,
+    LONG = 2,
+    UINT = 3,
+    UNINT64 = 4,
+    STRING = 5,
+    BUFFER = 6,
+    BUFFER_ARRAY = 7
+};
+
+#endif
