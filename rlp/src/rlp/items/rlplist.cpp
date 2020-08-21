d9c11df8fe33a4f518445de9679415eec538d581
Rahul Shelke <srahul07@gmail.com>
2020-08-21 19:35:36 +0530

    Changing type to rlpstring and adding code for each type

--- rlp/src/rlp/items/rlplist.cpp
+++ rlp/src/rlp/items/rlplist.cpp
@@ -1,12 +1,13 @@
 #include "rlplist.hpp"
 
 #include <utils/hex.hpp>
+#include <constants/elementtype.hpp>
 
-verified::rlp::RLPList::RLPList(const buffer_array_t input) {
+verified::rlp::RLPList::RLPList(const std::vector<verified::rlp::RLPString> input) {
     _bytes = input;
 }
 
-buffer_array_t verified::rlp::RLPList::GetBytes() {
+std::vector<verified::rlp::RLPString> verified::rlp::RLPList::GetBytes() {
     return _bytes;
 }
 
@@ -14,22 +15,123 @@ buffer_array_t verified::rlp::RLPList::GetBytes() {
 //     return BytesToString(_bytes);
 // }
 
-verified::rlp::RLPList verified::rlp::RLPList::Create(const buffer_t& input) {
-    buffer_array_t test_;
-    test_.push_back(input);
-    return RLPList(test_);
+verified::rlp::RLPString convertToRLPString(const variant_t& input) {
+    verified::rlp::RLPString bytes_;
+    switch (input.which()) {
+        case ElementType::CHAR: {
+            auto inpt_ = boost::get<char>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        case ElementType::INT: {
+            auto inpt_ = boost::get<int>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        case ElementType::LONG: {
+            auto inpt_ = boost::get<long>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        case ElementType::UINT: {
+            auto inpt_ = boost::get<uint_t>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        case ElementType::UNINT64: {
+            auto inpt_ = boost::get<uint64_t>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        case ElementType::STRING: {
+            auto inpt_ = boost::get<std::string>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        case ElementType::BUFFER: {
+            auto inpt_ = boost::get<buffer_t>(input);
+            bytes_ = verified::rlp::RLPString::Create(inpt_);
+            break;
+            }
+        // case ElementType::BUFFER_ARRAY: {
+        //     auto inpt_ = boost::get<buffer_array_t>(input);
+        //     bytes_ = verified::rlp::RLPString::Create(inpt_);
+        //     break;
+        //     }
+        default:
+            // TODO add error
+            break;
+        }
+
+        return bytes_;
 }
 
-// verified::rlp::RLPList verified::rlp::RLPList::Create(const buffer_array_t& input) {
-//     if(input.empty()) {
-//         // buffer_t empty_byte_ = verified::utils::EmptyByte();
-//         buffer_array_t empty_byte_;
-//         empty_byte_.push_back(verified::utils::EmptyByte());
-//     }
+verified::rlp::RLPList verified::rlp::RLPList::Create(const std::vector<variant_t>& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString bytes_;
+    for (size_t i = 0; i < input.size(); i++) {
+        bytes_ = convertToRLPString(input.at(i));
+        bytes_list_.push_back(bytes_);
+    }
+    return RLPList(bytes_list_);
+}
 
+verified::rlp::RLPList verified::rlp::RLPList::Create(const std::vector<char>& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString item;
+    for (size_t i = 0; i < input.size(); i++) {
+        item = verified::rlp::RLPString::Create(input.at(i));
+        bytes_list_.push_back(item);
+    }
+    return RLPList(bytes_list_);
+}
 
-// }
+verified::rlp::RLPList verified::rlp::RLPList::Create(const std::vector<int>& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString item;
+    for (size_t i = 0; i < input.size(); i++) {
+        item = verified::rlp::RLPString::Create(input.at(i));
+        bytes_list_.push_back(item);
+    }
+    return RLPList(bytes_list_);
+}
 
-// verified::rlp::RLPList verified::rlp::RLPList::Create(const variant_t& input) {
-    
-// }   
+verified::rlp::RLPList verified::rlp::RLPList::Create(const std::vector<long>& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString item;
+    for (size_t i = 0; i < input.size(); i++) {
+        item = verified::rlp::RLPString::Create(input.at(i));
+        bytes_list_.push_back(item);
+    }
+    return RLPList(bytes_list_);
+}
+
+verified::rlp::RLPList verified::rlp::RLPList::Create(const buffer_t& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString item;
+    for (size_t i = 0; i < input.size(); i++) {
+        item = verified::rlp::RLPString::Create(input.at(i));
+        bytes_list_.push_back(item);
+    }
+    return RLPList(bytes_list_);
+}
+
+verified::rlp::RLPList verified::rlp::RLPList::Create(const std::vector<uint64_t>& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString item;
+    for (size_t i = 0; i < input.size(); i++) {
+        item = verified::rlp::RLPString::Create(input.at(i));
+        bytes_list_.push_back(item);
+    }
+    return RLPList(bytes_list_);
+}
+
+verified::rlp::RLPList verified::rlp::RLPList::Create(const std::vector<std::string>& input) {
+    std::vector<verified::rlp::RLPString> bytes_list_;
+    verified::rlp::RLPString item;
+    for (size_t i = 0; i < input.size(); i++) {
+        item = verified::rlp::RLPString::Create(input.at(i));
+        bytes_list_.push_back(item);
+    }
+    return RLPList(bytes_list_);
+}
--- rlp/src/rlp/items/rlplist.hpp
+++ rlp/src/rlp/items/rlplist.hpp
@@ -4,19 +4,29 @@
 #include <string>
 
 #include <constants/alias.hpp>
+#include <rlp/items/rlpstring.hpp>
 
 namespace verified {
     namespace rlp {
         class RLPList {
             private:
-                buffer_array_t _bytes;
-                RLPList(buffer_array_t bytes);
+                std::vector<verified::rlp::RLPString> _bytes;
+                RLPList(std::vector<verified::rlp::RLPString> bytes);
             public:
-                buffer_array_t GetBytes();
+                std::vector<verified::rlp::RLPString> GetBytes();
                 // std::string AsHexString();
+                // static RLPList Create(const buffer_t& input);
+                // static RLPList Create(const buffer_array_t& input);
+                
+
+                // Individual data types
+                static RLPList Create(const std::vector<variant_t>& input);
+                static RLPList Create(const std::vector<char>& input);
+                static RLPList Create(const std::vector<int>& input);
+                static RLPList Create(const std::vector<long>& input);
                 static RLPList Create(const buffer_t& input);
-                static RLPList Create(const buffer_array_t& input);
-                static RLPList Create(const variant_t& input);
+                static RLPList Create(const std::vector<uint64_t>& input);
+                static RLPList Create(const std::vector<std::string>& input);
         };
     }
 }
