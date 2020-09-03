086bab9aa8c14604725164c0c03bfb507dd0b0bc
Rahul Shelke <srahul07@gmail.com>
2020-09-03 20:11:08 +0530

    Adding tests

---
+++ tests/src/test.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include <utils/hex.hpp>
+
+// TEST(StringToByetTest, Zero) {
+//   EXPECT_EQ(00000000, verified::utils::StringToBytes("").data());
+//   EXPECT_TRUE(true);
+// }
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  return RUN_ALL_TESTS();
+}
