d04ded28d535d57b3a4fbc0000a0c1d759460fd5
Rahul Shelke <srahul07@gmail.com>
2020-11-07 21:21:47 +0530

    Adding latest structure changes, not working yet

--- tests/src/utils/stringtobytestest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include <utils/hex.hpp>
-
-
-TEST(StringToByetTest, PositiveTest) {
-  EXPECT_EQ(00000000, verified::utils::StringToBytes("").data());
-  EXPECT_TRUE(true);
-}
