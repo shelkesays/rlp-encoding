1b2e32e2d8ef9562b00b5668e20db60f96c333f1
Rahul Shelke <srahul07@gmail.com>
2020-11-07 16:46:41 +0530

    Adding travis configurations and changing test case directory

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 rm -rf _build
 mkdir _build
-cd _build
-cmake ..
-make all
+# cd _build
+# cd _build
+# make all
+cmake -H. -B_build -DCMAKE_INSTALL_PREFIX=_install
+cmake --build _build
+# make
+# make install
