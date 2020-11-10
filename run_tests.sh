186270570e422706a047c9b6207978ca04388499
Rahul Shelke <srahul07@gmail.com>
2020-11-10 17:55:59 +0530

    Add shell script to run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,29 @@
-rm -rf _build
-mkdir _build
-# cd _build
-# cd _build
-# make all
-cmake -H. -B_build -DCMAKE_INSTALL_PREFIX=_install
-cmake --build _build
-# make
-# make install
+BUILD_DIR="_build"
+BIN_DIR="bin"
+LIBS_DIR="libs"
+
+# Delete existing build, bin and libs directory
+rm -rf "$BUILD_DIR"
+rm -rf "$BIN_DIR"
+rm -rf "$LIBS_DIR"
+
+# Create a new build directory
+mkdir "$BUILD_DIR"
+
+# Create a build
+cmake -H. -B"$BUILD_DIR"  # -DCMAKE_INSTALL_PREFIX=_install
+
+# Run build
+cmake --build "$BUILD_DIR"
+
+# Run unit test cases.
+echo "Running Unit Test Cases: "
+TEST_DIR="${BIN_DIR}/tests"
+if [ -d "$TEST_DIR" ]; then
+    echo "[SUCCESS]: Utils Test Case Execution starts: "
+    ./"$TEST_DIR"/utilstest
+    echo "[SUCCESS]: Utils Test Case Execution ends: "
+else
+    echo "[ERROR]: Test directory is not generated."
+fi
+echo "Unit Test Case execution complete."
